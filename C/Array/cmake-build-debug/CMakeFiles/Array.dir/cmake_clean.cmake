be90036b327123c1f87a7d2312427d4bd3706342
shriaas2898 <aasthashrivastava410@gmail.com>
2019-08-27 00:02:02 +0530

    added files

---
+++ C/Array/cmake-build-debug/CMakeFiles/Array.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Array.dir/array.c.o"
+  "CMakeFiles/Array.dir/home/shriaas/.CLion2019.2/config/scratches/array_test.c.o"
+  "libArray.pdb"
+  "libArray.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Array.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
