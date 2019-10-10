ceef9f37cf6dd9e5d43415156b0d97f20d0059f2
shriaas2898 <aasthashrivastava410@gmail.com>
2019-10-10 15:50:17 +0530

    Added array for matching delimiters

--- Compiler/stackHeader2.c
+++ Compiler/stackHeader2.c
@@ -1,4 +1,51 @@
 //
 // Created by shriaas on 10/10/19.
 //
+#include <stdio.h>
+#include "stackHeader2.h"
+
+void printStack(STACK stack)
+{
+    for(int index=0;index<10;index++)
+    {
+        printf("%c,",stack.bytes[index]);
+    }
+    printf("\b\n");
+}
+
+
+//Push Function of the Stack
+int push(STACK *stack,char ch)
+{
+    if(stack->top == (sizeof(stack->bytes)-1))
+        return FALSE;
+    else
+    {
+        stack->bytes[stack->top] = ch;
+        (stack->top)++;
+        return TRUE;
+    }
+}
+
+
+
+char isEmpty(STACK stack)
+{
+    if(stack.top == 0)
+        return TRUE;
+    else
+        return FALSE;
+}
+
+char pop(STACK *stack)
+{
+    if(isEmpty(*stack))
+        return FALSE;
+    else
+    {
+        char ch= stack->bytes[--(stack->top)];
+        return ch;
+    }
+}
+
 
--- Compiler/testCompiler.c
+++ Compiler/testCompiler.c
@@ -2,3 +2,79 @@
 // Created by shriaas on 10/10/19.
 //
 
+#include <stdio.h>
+#include "stackHeader2.h"
+/*
+ * search(array,key) -> index of key
+ */
+int search(char arr[],char key){
+    //
+    for (int i = 0; i <= sizeof(arr)/sizeof(int); ++i) {
+            if(key == arr[i])
+                return i;
+
+    }
+    return -1;
+
+}
+int main(){
+    STACK stack;
+    stack.top = 0;
+    FILE *fp;
+    char ch,value,flag=TRUE,previous=0;
+    char opening[] ={'(','{','[','\'','"'}, closing[]={')','}',']','\'','"'};
+    int position;
+
+    //opening file in read mode
+    fp=fopen("test.txt","r");
+    ch=fgetc(fp);
+    while(ch!=EOF)
+    {
+        position =search(opening,ch);
+
+        //pushing opening bracket
+
+        if(position!= -1)
+        {
+            
+            if(previous != '\'') {
+                
+                value = push(&stack, ch);
+            }
+            else
+                value = FALSE;
+
+        }
+        //poping when matching closing bracket is encountered
+        else if((position = search(closing,ch))!=-1 && previous != '\'')
+        {
+            if (isEmpty(stack))
+             {
+                        printf("Error in Program");
+                        flag=FALSE;
+                    }
+                    else if(pop(&stack) != opening[position])
+                    {
+                        
+                        printf("\n1.Error in Program ");
+                        flag=FALSE;
+                    }
+
+        }
+        if(!(flag))
+            break;
+        previous=ch;
+        ch=fgetc(fp);
+    }
+
+
+    fclose(fp);
+
+    if(isEmpty(stack) && flag==TRUE)
+        printf("Successfully Complied \n");
+    else if(flag==TRUE)
+        printf("\nError in Program \n");
+
+
+    return 0;
+}
